22094282f53d3086d5055d59d394fc946bc12d0c
jason-blankenship <156457375+jason-blankenship@users.noreply.github.com>
2024-12-04 17:15:05 -0600

    Added Gun, PlayerShoot, and Reload scripts - Jason

---
+++ CIS122 Project/Assets/Scripts/Scriptable Objects/GunData.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Gun", menuName = "Weapon/Gun")]
+public class GunData : ScriptableObject
+{
+
+
+    [Header("Info")]
+    public new string name;
+
+    [Header("Shooting")]
+    public float damage;
+    public float maxDistance;
+
+    [Header("Reloading")]
+    public int currentAmmo;
+    public int magSize;
+    public float fireRate;          // in rounds per minute
+    public float reloadTime;
+
+    [HideInInspector]
+    public bool reloading;
+
+}
