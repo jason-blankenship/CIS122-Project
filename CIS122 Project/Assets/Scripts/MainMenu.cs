d8961ef636592984b2e16c38b21397c7067c2392
LethroGH <devon.mckinney@mnsu.edu>
2024-12-10 02:15:47 -0600

    script updates

--- CIS122 Project/Assets/Scripts/MainMenu.cs
+++ CIS122 Project/Assets/Scripts/MainMenu.cs
@@ -1,80 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject pauseMenu;
 
-    public SceneManager Scene;
-    public GameObject myGameObjCanvas;
-    public KeyCode pauseKey = KeyCode.Escape;
-    // Start is called before the first frame update
     public void Start()
     {
-        myGameObjCanvas = GameObject.Find("Pause");
-        myGameObjCanvas.SetActive(false);
-    
-        myGameObjCanvas = GameObject.Find("MainMenu");
+        mainMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+
         Time.timeScale = 0;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
-        myGameObjCanvas.SetActive(true);
-
-
     }
-    public void Update()
-    {
-        if (Input.GetKey(pauseKey))
-        {
-            Pause();
-        }
-        else if(Input.GetKey(pauseKey) && myGameObjCanvas)
-        {
-            UnPause();
-        }
-    }
+
     public void PlayGame()
     {
-        
-        myGameObjCanvas = GameObject.Find("MainMenu");
-        myGameObjCanvas.SetActive(false);
+        mainMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+
         Time.timeScale = 1;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        SceneManager.SetActiveScene(); //taking active scene getting the build index adding one then loading that
-        
     }
 
-    public void QuitGame()
+    public void Pause()
     {
-        Application.Quit();
-    }
-
-    public void Pause() 
-    {
-        SceneManager.UnloadSceneAsync("SampleScene");
-        SceneManager.LoadScene("StartScreen");
-        myGameObjCanvas = GameObject.Find("Pause");
-        myGameObjCanvas.SetActive(true);
+        mainMenu.SetActive(false);
+        pauseMenu.SetActive(true);
 
         Time.timeScale = 0;
-
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
 
     public void UnPause()
-    { 
-        myGameObjCanvas = GameObject.Find("Pause");
-        myGameObjCanvas.SetActive(false);
-        SceneManager.UnloadSceneAsync("StartScreen");
+    {
 
-        SceneManager.LoadScene("SampleScene");
+        pauseMenu.SetActive(false);
 
+        Time.timeScale = 1;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        Time.timeScale = 1;
+    }
+
+    public void ReturnToMainMenu()
+    {
+        mainMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
